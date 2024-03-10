4ab021b6da41e61b5267d50ed9b6508563bebb32
marcho <madergilev@edu.hse.ru>
2024-03-10 13:49:15 +0300

    added commands

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import asyncio
 import logging
 import sys
 
-from aiogram import Router
-from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand
+from aiogram import Bot, Dispatcher, Router
 
+import helpers.helpers
 from helpers import config
 
 from routers.main_router import main_r
@@ -20,11 +21,12 @@ edit = Router()
 async def main():
     bot = Bot(config.BOT_TOKEN)
     dp = Dispatcher()
+    dp.include_routers(main_r, payment, confirm)
+    bot_cmd = []
+    for cmd in helpers.helpers.bot_command:
+        bot_cmd.append(BotCommand(command=cmd[0], description=cmd[1]))
 
-    dp.include_router(main_r)
-    dp.include_router(payment)
-    dp.include_router(confirm)
-
+    await bot.set_my_commands(bot_cmd)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
