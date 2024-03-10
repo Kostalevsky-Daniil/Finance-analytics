c2c693ae4bd81d8e68f84fd000fe98f4eae2edb5
naziikk <zacnazar20@gmail.com>
2024-03-10 23:03:40 +0300

    pre last commit

--- StatesAuthor.py
+++ StatesAuthor.py
@@ -1,11 +1,22 @@
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 class GlobalStates(StatesGroup):
-    description = State()
+    chat_name = State()
+    confirmation_chat_name = State()
+    limit_of_users = State()
+    confirmation_limit_of_users = State()
     price_per_month = State()
-    limitOfUsers = State()
+    confirmation_price = State()
+    description = State()
+    confirmation_description = State()
     waiting_for_actions = State()
 
+
+class changingStates(StatesGroup):
+    choosing_existing_community = State()
+    writing_new_description = State()
+    setting_new_limitOfUsers = State()
+    setting_new_price_per_month = State()
 
 def make_row_keyboard(items: list[str]) -> ReplyKeyboardMarkup:
     row = [KeyboardButton(text=item) for item in items]
--- connectUser.py
+++ connectUser.py
@@ -1,29 +1,23 @@
 import random
-import logging
-import AuthorLog
-from aiogram import Bot, Dispatcher, types, F
-from aiogram.filters.command import Command
+from aiogram.filters import StateFilter
+from aiogram import F, Router
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-# dif
-
-from aiogram.types import Message, LabeledPrice, PreCheckoutQuery, \
-    ReplyKeyboardRemove, ContentType
+from aiogram.types import Message
 from aiogram import Bot, Dispatcher, types
 from aiogram.fsm.context import FSMContext
-from aiogram.filters import CommandStart, Command, CommandObject, StateFilter
+from aiogram.filters import Command
 import logging
 import asyncio
-import StatesAuthor
-from StatesAuthor import arr1
-from StatesAuthor import GlobalStates
+from StatesAuthor import GlobalStates, changingStates
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token="YOUR_BOT_TOKEN")
+bot = Bot(token="6733664975:AAFvr0jrCGHjhSlg0LM1zCmmHSevjsB96E8")
 dp = Dispatcher()
 # Dictionary to store unique links for chats
 unique_links = {}
+
 
 # Function to generate a unique chat link
 async def generate_unique_link(chat_id):
@@ -31,10 +25,15 @@ async def generate_unique_link(chat_id):
     unique_links[chat_id] = link
     return link
 
+
 # Function to check if a user has a paid subscription
 async def is_subscription_paid(user_id):
     # Implement logic for checking subscription payment for a specific user
     return True
+
+
+routerForOwner = Router()
+
 
 @dp.message(F.text, Command("start"))
 async def cmd_start(message: types.Message):
@@ -53,12 +52,13 @@ async def cmd_start(message: types.Message):
     await message.answer("1. Are you the owner?\n"
                          "2. Do you want to join the chat?", reply_markup=one_time_keyboard)
 
+
 @dp.message(F.text.lower() == "owner")
 async def author(message: types.Message):
     # Keyboard for choosing the action for the owner
     one_time_keyboard = ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="New community"), KeyboardButton(text="Existed community")],
+            [KeyboardButton(text="New community"), KeyboardButton(text="Existing community")],
         ],
         one_time_keyboard=True,
         resize_keyboard=True,
@@ -66,24 +66,167 @@ async def author(message: types.Message):
     await message.answer("1. Do you want to register a new community?\n"
                          "2. Do you want to edit community INFO?", reply_markup=one_time_keyboard)
 
-@dp.message(F.text.lower() == "new community")
-async def new_com(message: types.Message):
-    await message.reply("!")
 
-@dp.message(F.text.lower() == "existed community")
-async def exist_com(message: types.Message):
+# -----------------------------------------------------------#new community#--------------------------------------------------------------------
+
+@dp.message(F.text.lower() == "new community")
+async def start_new_community(message: types.Message, state: FSMContext):
+    one_time_keyboard = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="Cancel")],
+        ],
+        one_time_keyboard=True,
+        resize_keyboard=True,
+    )
+
+    await message.reply("To register a new community, please follow these steps:\n"
+                        "1. Add me to your group as an administrator.\n"
+                        "2. Grant me the rights to invite new members.\n"
+                        "3. Send the name of your community.", reply_markup=one_time_keyboard)
+    await state.set_state(GlobalStates.chat_name)
+
+
+@dp.message(F.text.lower() == 'cancel', StateFilter(GlobalStates.chat_name))
+async def cancel_registration(message: types.Message, state: FSMContext):
+    await state.clear()
+    await message.reply("Community registration canceled.")
+
+
+@dp.message(StateFilter(GlobalStates.chat_name))
+async def confirm_add_bot(message: types.Message, state: FSMContext):
+    community_name = message.text.lower()
+    # check if the bot is administrator
+    # chat_member = await bot.get_chat_member(message.chat.id, bot.id)
+    chat_member = "administrator"
+    if chat_member == "administrator":  # chat_member.status
+        await message.reply(f"Great! Community name set to {community_name}. Now, set the limit of users.")
+        await state.set_state(GlobalStates.limit_of_users)
+    else:
+        await message.reply("Please add the bot as an administrator to proceed.")
+
+
+@dp.message(F.text, StateFilter(GlobalStates.limit_of_users))
+async def get_limit(message: types.Message, state: FSMContext):
+    limit_of_users = message.text
+
+    # Check if limit_of_users is a positive integer
+    if limit_of_users.isdigit() and int(limit_of_users) > 0:
+        # Ask for confirmation
+        confirmation_keyboard = ReplyKeyboardMarkup(
+            keyboard=[
+                [KeyboardButton(text="Confirm"), KeyboardButton(text="Cancel")],
+            ],
+            one_time_keyboard=True,
+            resize_keyboard=True,
+        )
+
+        await message.reply(f"Limit of users: {limit_of_users}. Confirm or cancel?", reply_markup=confirmation_keyboard)
+        await state.update_data(limit_of_users=int(limit_of_users))
+        await state.set_state(GlobalStates.confirmation_limit_of_users)
+    else:
+        await message.reply("Invalid input. Please enter a positive integer for the limit of users.")
+
+
+@dp.message(F.text.lower() == 'confirm', StateFilter(GlobalStates.confirmation_limit_of_users))
+async def confirm_limit_of_users(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    limit_of_users = data.get("limit_of_users")
+    await message.reply(f"Limit of users set to {limit_of_users}. Now, set the price per month.")
+    await state.set_state(GlobalStates.price_per_month)
+
+
+@dp.message(F.text.lower() == 'cancel', StateFilter(GlobalStates.confirmation_limit_of_users))
+async def cancel_limit_of_users(message: types.Message, state: FSMContext):
+    await state.clear()
+    await message.reply("Community registration canceled.")
+
+
+@dp.message(F.text, StateFilter(GlobalStates.price_per_month))
+async def get_price(message: types.Message, state: FSMContext):
+    price = message.text
+
+    # Check if price is a positive integer
+    if price.isdigit() and int(price) > 0:
+        # Ask for confirmation
+        confirmation_keyboard = ReplyKeyboardMarkup(
+            keyboard=[
+                [KeyboardButton(text="Confirm"), KeyboardButton(text="Cancel")],
+            ],
+            one_time_keyboard=True,
+            resize_keyboard=True,
+        )
+
+        await message.reply(f"Price per month: {price}. Confirm or cancel?", reply_markup=confirmation_keyboard)
+        await state.update_data(price=int(price))
+        await state.set_state(GlobalStates.confirmation_price)
+    else:
+        await message.reply("Invalid input. Please enter a positive integer for the price per month.")
+
+
+@dp.message(F.text.lower() == 'confirm', StateFilter(GlobalStates.confirmation_price))
+async def confirm_price(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    price = data.get("price")
+    await message.reply(f"Price per month set to {price}. Now, set the channel description.")
+    await state.set_state(GlobalStates.description)
+
+
+@dp.message(F.text.lower() == 'cancel', StateFilter(GlobalStates.confirmation_price))
+async def cancel_price(message: types.Message, state: FSMContext):
+    await state.clear()
+    await message.reply("Community registration canceled.")
+
+
+@dp.message(F.text, StateFilter(GlobalStates.description))
+async def get_description(message: types.Message, state: FSMContext):
+    description = message.text
+
+    # Ask for confirmation
+    confirmation_keyboard = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="Confirm"), KeyboardButton(text="Cancel")],
+        ],
+        one_time_keyboard=True,
+        resize_keyboard=True,
+    )
+
+    await message.reply(f"Description: {description}. Confirm or cancel?", reply_markup=confirmation_keyboard)
+    await state.update_data(description=description)
+    await state.set_state(GlobalStates.confirmation_description)
+
+
+@dp.message(F.text.lower() == 'confirm', StateFilter(GlobalStates.confirmation_description))
+async def confirm_description(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    description = data.get("description")
+    await message.reply(f"Description set. Community registration is complete.")
+    await state.clear()
+
+
+@dp.message(F.text.lower() == 'cancel', StateFilter(GlobalStates.confirmation_description))
+async def cancel_description(message: types.Message, state: FSMContext):
+    await state.clear()
+    await message.reply("Community registration canceled.")
+
+
+# -----------------------------------------------------------#Existed#--------------------------------------------------------------------
+
+@dp.message(F.text.lower() == "existing community")
+async def existed_com(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
 
     # Dummy logic for checking if the person is the owner
     is_owner = True
     if is_owner:
         await message.reply("Which community data do you want to edit? (enter the name)\n")
+        await state.set_state(changingStates.choosing_existing_community)
         # Get the user's information
     else:
         await message.reply("You are not the owner of any community.")
 
-@dp.message(F.text)
-async def handle_community_name(message: types.Message):
+
+@dp.message(StateFilter(changingStates.choosing_existing_community), F.text)
+async def handle_community_name(message: types.Message, state: FSMContext):
     community_name = message.text
     user_id = message.from_user.id
 
@@ -92,73 +235,44 @@ async def handle_community_name(message: types.Message):
 
     if is_owner:
         await message.answer(f"You are the owner of the community '{community_name}'. ")
-        # Keyboard for choosing the field to edit
-        one_time_keyboard = ReplyKeyboardMarkup(
-            keyboard=[
-                [KeyboardButton(text="Description"), KeyboardButton(text="Count of people"),
-                 KeyboardButton(text="Price per month")],
-            ],
-            one_time_keyboard=True,
-            resize_keyboard=True,
-        )
-        await message.answer("Choose the field for editing:", reply_markup=one_time_keyboard)
+        await message.answer("Enter new description.")
+        await state.set_state(changingStates.writing_new_description)
     else:
         await message.answer(f"You are not the owner of the community '{community_name}'.")
 
-@dp.message(F.text.lower() == "description")
+
+@dp.message(StateFilter(changingStates.writing_new_description), F.text)
 async def set_new_community_description(message: types.Message, state: FSMContext):
-    await message.answer("Enter the new description:")
-    await state.set_state(GlobalStates.writing_new_description)
+    await state.update_data(new_description=message.text.lower())
+    await state.set_state(changingStates.setting_new_limitOfUsers)
+    await message.answer(text="Brilliant, description had been changed. Now set new limit of people.")
 
-@dp.message(
-    GlobalStates.writing_new_description,
-    F.text
-)
-async def done_set_new_description(message: Message, state: FSMContext):
-    await state.update_data(description=message.text.lower())
-    await message.answer(
-        # request to database (set new INFO)
-        text="Description had been changed.",
-    )
-    await state.set_state(GlobalStates.setting_new_limitOfUsers)
 
-@dp.message(F.text.lower() == "count of people")
+@dp.message(StateFilter(changingStates.setting_new_limitOfUsers), F.text)
 async def set_new_community_count_of_people(message: types.Message, state: FSMContext):
-    await message.answer("Enter the new count of people:")
-    await state.set_state(GlobalStates.setting_new_limitOfUsers)
+    if message.text.isdigit():
+        await state.update_data(count_of_people=message.text.lower())
+        await state.set_state(changingStates.setting_new_price_per_month)
+        await message.answer(text="Nice, count of people had been changed. Now set new prices per month.")
+    else:
+        await message.answer(text="You entered not integer value. Try again!")
 
-@dp.message(
-    GlobalStates.setting_new_limitOfUsers,
-    F.text.isdigit()
-)
-async def done_set_new_count_of_people(message: Message, state: FSMContext):
-    await state.update_data(count_of_people=message.text.isdigit())
-    # request to database (set new INFO)
-    await message.answer(
-        text="Count had been changed.",
-    )
-    await state.set_state(GlobalStates.setting_new_price_per_month)
 
-@dp.message(F.text.lower() == "price per month")
+@dp.message(StateFilter(changingStates.setting_new_price_per_month), F.text)
 async def set_new_community_price_per_month(message: types.Message, state: FSMContext):
-    await message.answer("Enter the new price per month:")
-    await state.set_state(GlobalStates.setting_new_price_per_month)
+    if message.text.isdigit():
+        await state.update_data(price_month=message.text.lower())
+        await state.set_state(changingStates.setting_new_price_per_month)
+        await message.answer(text="Price per month had been changed.")
+        await state.clear()
+    else:
+        await message.answer(text="You entered not integer value. Try again!")
 
-@dp.message(
-    GlobalStates.setting_new_price_per_month,
-    F.text.isdigit()
-)
-async def done_set_new_price_per_month(message: Message, state: FSMContext):
-    await state.update_data(price_per_month=message.text.isdigit())
-    # request to database (set new INFO)
-    await message.answer(
-        text="Price had been changed.",
-    )
-    await state.clear()
 
 @dp.message(F.text.lower() == "member")
 async def member(message: types.Message):
     await message.reply("!")
+
 
 # Bot functions
 @dp.message(Command("help"))
@@ -167,10 +281,13 @@ async def help_user(message: types.Message):
                          "2. /join_chat - Join a private chat by providing the chat ID.\n"
                          "3. /help - Display this help message.")
 
+
 @dp.message(Command("join_chat"))
 async def join_chat(message: types.Message):
     # Ask the user for the chat ID
     await message.answer("Please provide the chat ID you want to join. (Enter any digits, will be changed)")
+
+
 async def process_chat_id(message: types.Message):
     # Assume the user provided the chat ID
     chat_id_to_join = message.text
@@ -189,9 +306,11 @@ async def process_chat_id(message: types.Message):
     else:
         await message.answer('To gain access to a private chat, pay a monthly subscription.')
 
+
 @dp.message(F.text.isdigit())
 async def process_chat_id_wrapper(message: types.Message):
     await process_chat_id(message)
+
 
 # Random responses for unrecognized commands or messages
 random_responses = [
@@ -202,6 +321,7 @@ random_responses = [
     "I apologize, but I'm just a bot and don't have that functionality. Can I help with something else?",
 ]
 
+
 # Handler for random messages
 @dp.message(F.text)
 async def process_message(message: types.Message):
@@ -209,11 +329,16 @@ async def process_message(message: types.Message):
     await message.reply(response)
     await message.answer("Enter /help; I can show you my abilities.")
 
+
 # Function to check if the chat ID is valid (you can add your own validation logic here)
 def is_valid_chat_id(chat_id):
     return True
+
+
 # Start the bot
 async def main():
     await dp.start_polling(bot)
+
+
 if __name__ == "__main__":
     asyncio.run(main())
