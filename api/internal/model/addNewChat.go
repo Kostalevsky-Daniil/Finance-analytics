b8bdd1e951acfa9f2dd40dfc63b49f6e5e840f87
Kostalevsky-Daniil <daniilkostalevsky@yandex.ru>
2024-03-11 17:23:25 +0300

    fix: addNewChat.go

--- api/internal/model/addNewChat.go
+++ api/internal/model/addNewChat.go
@@ -7,11 +7,3 @@ type AddNewChat struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 }
-
-// {
-//     "chat_id": 1312312312,
-//     "owner_id": 12413413,
-//     "price": 1312,
-//     "name": "Порно 18+",
-//     "description": "Новое порно каждый день"
-// }
