f394637e2946c2a329a881e46237daebfde16131
marcho <madergilev@edu.hse.ru>
2024-03-11 13:31:40 +0300

    latest from mark

--- routers/create_router.py
+++ routers/create_router.py
@@ -48,7 +48,7 @@ async def get_limit(message: types.Message, state: FSMContext):
 async def confirm_limit_of_users(message: types.Message, state: FSMContext):
     data = await state.get_data()
     limit_of_users = data.get("limit_of_users")
-    await message.reply(f"Limit of users set to {limit_of_users}. Now, set the price per month.")
+    await message.reply(f"Limit of users set to {limit_of_users}. Now, set the price per month in USD.")
     await state.set_state(GlobalStates.price_per_month)
 
 
@@ -67,7 +67,7 @@ async def get_price(message: types.Message, state: FSMContext):
             resize_keyboard=True,
         )
 
-        await message.reply(f"Price per month: {price}. Confirm or cancel?", reply_markup=confirmation_keyboard)
+        await message.reply(f"Price per month: {price} USD. Confirm or cancel?", reply_markup=confirmation_keyboard)
         await state.update_data(price=int(price))
         await state.set_state(GlobalStates.confirmation_price)
     else:
@@ -78,7 +78,7 @@ async def get_price(message: types.Message, state: FSMContext):
 async def confirm_price(message: types.Message, state: FSMContext):
     data = await state.get_data()
     price = data.get("price")
-    await message.reply(f"Price per month set to {price}. Now, set the channel description.")
+    await message.reply(f"Price per month set to {price} USD. Now, set the channel description.")
     await state.set_state(GlobalStates.description)
 
 
